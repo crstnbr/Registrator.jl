c438b0cb7db6340ad5f1486d96677f728b9c19f5
Gunnar Farnebäck <gunnar.farneback@contextvision.se>
2019-04-17 14:17:58 +0200

    Export create_registry and register.

--- src/Registrator.jl
+++ src/Registrator.jl
@@ -5,6 +5,8 @@ using UUIDs, LibGit2, DataStructures
 import Base: PkgId
 import Pkg: Pkg, TOML, GitTools
 
+export create_registry, register
+
 const DEFAULT_REGISTRY = "https://github.com/JuliaRegistries/General"
 const REGISTRIES = Dict{String,UUID}()
 
